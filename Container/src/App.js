77a148a66b63cde97ff36008dc2405be39f83f2b
Siddhant Rath <sid@tamu.edu>
2024-02-06 16:49:54 -0600

    intrim

--- Container/src/App.js
+++ Container/src/App.js
@@ -3,13 +3,16 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { PrimeReactProvider } from "primereact/api";
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import ChemSphereApp from "./components/ChemSphereApp";
+import TitleBar from "./components/TitleBar";
 
 export default () => {
   return (
-    <PrimeReactProvider>
-      <h1>Container</h1>
-      <hr />
-      <ChemSphereApp />
-    </PrimeReactProvider>
+    <BrowserRouter>
+      <PrimeReactProvider>
+        <TitleBar />
+        <hr />
+        <ChemSphereApp />
+      </PrimeReactProvider>
+    </BrowserRouter>
   );
 };
